3e4f8ec2ad92b7f1cf57ce6f19b0b1a38a1d9a3d
Victor Ferrao Santolim <victorfsantolim@gmail.com>
2018-03-22 23:43:16 -0300

    Versao final

--- src/ccc_lib.c
+++ src/ccc_lib.c
@@ -1,39 +1,44 @@
+//Victor Ferrao Santolim
+//RA 187888
+
+//Funcoes em C para o Lex que conversor de CamelCase
 
 #include "ccc_lib.h"
 #include <string.h>
-#include <stdio.h>
 
 void camelcase_to_underline(char *input, char *output) {
 
 	int i=0;
-	int numUnder=0;
+	int numUnder=0; //Numero de underlines detectados
+	int tamInput = strlen(input); //Tamanho do input
 
-	int tamInput = strlen(input);
-
-	strcpy(output, "");
-	//printf("Input=|%s|\n", input);
-	//printf("Output=|%s|\n", output);
-
+	strcpy(output, ""); //Limpa a string input
+	
+	//Converte a primeira letra para minuscula
 	output[0] = input[0] + 32;
 
+	//Percorre o vetor de entrada	
 	for(i = 1; i < tamInput ; i++)
 	{
 
+		//Se achar maiuscula
 		if(input[i] >= 65 && input[i] <= 90)
 		{
-		
+			//Troca por underline
 			output[i+numUnder] = '_';
 			numUnder ++;
+			//E adiciona a letra minuscula correspondente
 			output[i+numUnder] = input[i] + 32;
 		}
 		else
 		{
+			//Caso contrario apenas repita a letra
 			output[i+numUnder] = input[i];
 		}
 	}
 
+	//Null-terminate a string
 	output[i+numUnder] = '\0';
-
 
 	return;
 }
@@ -42,30 +47,34 @@ void underline_to_camelcase(char *input, char *output) {
 
 
 	int i=0;
+	int numUnder=0; //Numero de underlines gerados
+	int tamInput = strlen(input); //Tamanho do input
 
-	int numUnder=0;
-
-	int tamInput = strlen(input);
-	strcpy(output, "");
+	strcpy(output, ""); //Limpa a string input
 	
-
+	//Converte a primeira letra para maiuscula
 	output[0] = input[0] - 32;
 
+	//Percorre o vetor de entrada
 	for(i = 1; i < tamInput ; i++)
 	{
 
+		//Se achar um underline
 		if(input[i+numUnder] == '_')
 		{
 		
+			//Substitui pela proxima letra na versao maiuscula
 			numUnder ++;
 			output[i] = input[i+numUnder] - 32;
 		}
 		else
 		{
+			//Caso contrario apenas repete a letra			
 			output[i] = input[i+numUnder];
 		}
 	}
 
+	//Null-terminate a string
 	output[i-numUnder] = '\0';
 
  	return;
